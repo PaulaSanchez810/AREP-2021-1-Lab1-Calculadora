b9100187a0eb1d20bbea7058324e95e552556544
PaulaSanchez810 <paulasanchez810@gmail.com>
2021-01-30 20:48:02 -0500

    revision

--- Calculadora-AREP/src/main/java/edu/escuelaing/arep/App.java
+++ Calculadora-AREP/src/main/java/edu/escuelaing/arep/App.java
@@ -1,13 +1,37 @@
 package edu.escuelaing.arep;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+import edu.escuelaing.arep.LinkedliStstructure.LinkedList;
+
+public class App {
+    private static Calculator calculator = new Calculator();
+
+    public static void main( String[] args ) throws FileNotFoundException {
+        File file = new File(args[0]);
+        Scanner scaner = new Scanner(file);
+        while (scaner.hasNextLine()) {
+            String line =scaner.nextLine();
+            calculateData(line);
+        }
+        scaner.close();
+
+    }
+
+    private static void calculateData(String data) {
+        LinkedList<Double> linkedList = new LinkedList<>();
+        String[] dataSet = data.split(",");
+        for (String i : dataSet){
+            double value = Double.parseDouble(i);
+            linkedList.insertLast(value);
+        }
+        System.out.println("La media es: "+ calculator.calculateMean(linkedList));
+        System.out.println("La desviación estándar es: "+ calculator.calculateStandarDeviation(linkedList));
     }
 }
+
+
